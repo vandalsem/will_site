7039a7ed49cf5fee3c19623390c4e7914e4e4c06
vandalsem <vandalsemjj@gmail.com>
2023-12-20 20:09:45 -0800

    Can edit review now, and working on deleting it. Just can't get modal sorted out.

--- will_site.py
+++ will_site.py
@@ -45,6 +45,15 @@ def reviews():
     reviews = Reviews.objects.all()
     return render_template('reviews.html', reviews=reviews)
 
+@app.route('/review/<review_id>')
+def review_details(review_id):
+    review = Reviews.objects.get(id=review_id)
+    return render_template('review_details.html', review=review)
+
+########
+# CRUD #
+########
+
 @app.route('/create_review', methods=['GET','POST'])
 def create_review():
 
@@ -67,14 +76,31 @@ def create_review():
     
     return render_template('create_review.html', form=form)
 
+@app.route('/edit_review/<review_id>', methods=['GET','POST'])
+def edit_review(review_id):
+    review = Reviews.objects.get(id=review_id)
+    form = ReviewForm(request.form, obj=review)
+
+    if request.method == 'POST':
+        if 'cancel' in request.form:
+            return redirect(url_for('reviews'))
+        if form.validate_on_submit():
+            form.populate_obj(review)
+            review.save()
+            return redirect(url_for('reviews'))
+
+    return render_template('edit_review.html', form=form, review=review)
+
+@app.route('/delete_review/<review_id>')
+def delete_review(review_id):
+    review = Reviews.objects.get(id=review_id)
+    if review:
+        review.delete()
+    return redirect(url_for('reviews'))
+
 @app.route('/submit_review',methods=['GET','POST'])
 def submit_review():
     return render_template('submit_review.html')
-
-@app.route('/review/<review_id>')
-def review_details(review_id):
-    review = Reviews.objects.get(id=review_id)
-    return render_template('review_details.html', review=review)
 
 if __name__ == '__main__':
     app.run(debug=True)
